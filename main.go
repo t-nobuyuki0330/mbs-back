c978732af87c388d18489af0a92bbe7632622ef6
t-nobuyuki0330 <t.nobuyuki2002@gmail.com>
2023-04-25 14:07:10 +0000

    all allow cors

--- main.go
+++ main.go
@@ -4,37 +4,15 @@ import (
     "github.com/t-nobuyuki0330/mbs-back/controller"
     "github.com/gin-gonic/gin"
     "github.com/gin-contrib/cors"
-    "time"
+    _ "time"
 )
 
 func main() {
     router := gin.Default()
 
-    router.Use(cors.New(cors.Config{
-        // allow site
-        AllowOrigins: []string{
-            "https://funbook.pages.dev/",
-            "http://localhost:5173",
-        },
-        // allow http method
-        AllowMethods: []string{
-            "POST",
-            "GET",
-        },
-        // allow http request header
-        AllowHeaders: []string{
-            "Access-Control-Allow-Credentials",
-            "Access-Control-Allow-Headers",
-            "Content-Type",
-            "Content-Length",
-            "Accept-Encoding",
-            "Authorization",
-        },
-        // info
-        AllowCredentials: true,
-        // preflight request cache time
-        MaxAge: 24 * time.Hour,
-    }))
+    config := cors.DefaultConfig()
+    config.AllowAllOrigins = true
+    router.Use(cors.New(config))
 
     router.POST("/funbook/api/search", controller.SearchFunctions)
 
