d31e36d03e07778cea6dffd28ba2f91fd77440fb
t-nobuyuki0330 <t.nobuyuki2002@gmail.com>
2023-04-25 14:38:38 +0000

    all allow cors

--- main.go
+++ main.go
@@ -3,16 +3,16 @@ package main
 import (
     "github.com/t-nobuyuki0330/mbs-back/controller"
     "github.com/gin-gonic/gin"
-    _ "github.com/gin-contrib/cors"
+    "github.com/gin-contrib/cors"
     _ "time"
 )
 
 func main() {
     router := gin.Default()
 
-    // config := cors.DefaultConfig()
-    // config.AllowAllOrigins = true
-    // router.Use(cors.New(config))
+    config := cors.DefaultConfig()
+    config.AllowAllOrigins = true
+    router.Use(cors.New(config))
 
     router.POST("/funbook/api/search", controller.SearchFunctions)
 
